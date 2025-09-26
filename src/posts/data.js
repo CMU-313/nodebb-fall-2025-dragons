bbdf0a0e5211405f50ef2054b586ee9622758f73
bschutzm <brianschutzman@gmail.com>
2025-09-26 18:33:35 +0000

    fixed bugs

--- src/posts/data.js
+++ src/posts/data.js
@@ -8,6 +8,7 @@ const intFields = [
 	'uid', 'pid', 'tid', 'deleted', 'timestamp',
 	'upvotes', 'downvotes', 'deleterUid', 'edited',
 	'replies', 'bookmarks', 'announces', 'public',
+	'answered',
 ];
 
 module.exports = function (Posts) {
