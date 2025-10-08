da95ad58a026df100293da33cacc1a658f5bd692
Evelyn Lui <evelynlu@andrew.cmu.edu>
2025-10-07 23:25:20 -0400

    feat: restrict answered to main posts and add filtering methods
    Only allow main posts to be marked as answered. Add methods to retrieve answered/unanswered posts with pagination and counts.

--- src/posts/answered.js
+++ src/posts/answered.js
@@ -30,11 +30,61 @@ module.exports = function (Posts) {
 
 	Posts.answered.mark = async (pid /*, uid */) => {
 		console.log('[answered] mark pid=%s', pid);
-		await Posts.answered.set(pid, true);
+		await Posts.answered.set(pid, true); 
 	};
 
 	Posts.answered.unmark = async (pid /*, uid */) => {
 		console.log('[answered] unmark pid=%s', pid);
 		await Posts.answered.set(pid, false);
+	};
+
+	// Get answered posts (main posts only)
+	Posts.answered.getAnswered = async function (start, stop, uid) {
+		const pids = await db.getSortedSetRevRange('posts:answered', start, stop);
+		return await Posts.getPostsByPids(pids, uid);
+	};
+
+	// Get unanswered posts (main posts that are not answered)
+	Posts.answered.getUnanswered = async function (start, stop, uid) {
+		// Get all main posts (from topics)
+		const allTids = await db.getSortedSetRange('topics:tid', 0, -1);
+		const mainPids = await Promise.all(
+			allTids.map(tid => db.getObjectField(`topic:${tid}`, 'mainPid'))
+		);
+		
+		// Filter out null/undefined mainPids
+		const validMainPids = mainPids.filter(pid => pid);
+		
+		// Get answered PIDs
+		const answeredPids = await db.getSortedSetMembers('posts:answered');
+		
+		// Find unanswered main posts
+		const unansweredPids = validMainPids.filter(pid => !answeredPids.includes(pid));
+		
+		// Paginate
+		const paginatedPids = unansweredPids.slice(start, stop + 1);
+		
+		return await Posts.getPostsByPids(paginatedPids, uid);
+	};
+
+	// Get posts by answered status (true = answered, false = unanswered)
+	Posts.answered.getByStatus = async function (answered, start, stop, uid) {
+		if (answered) {
+			return await Posts.answered.getAnswered(start, stop, uid);
+		} else {
+			return await Posts.answered.getUnanswered(start, stop, uid);
+		}
+	};
+
+	// Get count of answered posts
+	Posts.answered.getAnsweredCount = async function () {
+		return await db.sortedSetCard('posts:answered');
+	};
+
+	// Get count of unanswered posts
+	Posts.answered.getUnansweredCount = async function () {
+		const allTids = await db.sortedSetCard('topics:tid');
+		const answeredCount = await Posts.answered.getAnsweredCount();
+		return allTids - answeredCount;
 	};
 };
