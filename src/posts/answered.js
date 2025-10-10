8dcb7ced20928aa7b2e6a89bce69ec0a73a8f95d
zmahmud <zmahmud@andrew.cmu.edu>
2025-10-10 12:22:11 +0000

    Debugged schema based testcases

--- src/posts/answered.js
+++ src/posts/answered.js
@@ -7,7 +7,6 @@ module.exports = function (Posts) {
 	Posts.answered = Posts.answered || {};
 
 	Posts.answered.set = async (pid, answered) => {
-		console.log('[answered] set start pid=%s answered=%s', pid, answered);
 		const tid = await Posts.getPostField(pid, 'tid');
 		await db.setObjectField(`post:${pid}`, 'answered', answered ? 1 : 0);
 		if (answered) {
@@ -18,16 +17,13 @@ module.exports = function (Posts) {
 			await db.sortedSetRemove('posts:answered', pid);
 			await db.sortedSetRemove(`tid:${tid}:answered`, pid);
 		}
-		console.log('[answered] set done pid=%s', pid);
 	};
 
 	Posts.answered.mark = async (pid /*, uid */) => {
-		console.log('[answered] mark pid=%s', pid);
 		await Posts.answered.set(pid, true);
 	};
 
 	Posts.answered.unmark = async (pid /*, uid */) => {
-		console.log('[answered] unmark pid=%s', pid);
 		await Posts.answered.set(pid, false);
 	};
 };
